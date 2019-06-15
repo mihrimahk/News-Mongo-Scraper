100e03078c297d6bf7d86db72bbfc22c97d82fc3
Mihrimah K <mihrimahk@gmail.com>
2019-06-15 15:13:13 -0400

    update for typo

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
+
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsScrape";
 
