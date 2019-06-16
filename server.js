12a40b2cbe1c5fc32a4a5ea2929095e368153806
Mihrimah K <mihrimahk@gmail.com>
2019-06-15 22:40:21 -0400

    fixed htlm, writing read me

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get("/scrape", function(req, res) {
     });
 
     // Send a message to the client
-    res.send("Scrape Complete");
+    res.send("Scrape Complete <a href='/'>Go to Articles!</a>");
   });
 });
 
